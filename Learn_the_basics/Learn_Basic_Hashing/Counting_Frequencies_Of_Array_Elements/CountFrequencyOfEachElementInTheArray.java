3c919fa1bab1551ac9cced300293376e55bcdaaf
Divya-Rasam <165023802+Divya-Rasam@users.noreply.github.com>
2026-03-18 17:05:58 +0530

    8

---
+++ Learn_the_basics/Learn_Basic_Hashing/Counting_Frequencies_Of_Array_Elements/CountFrequencyOfEachElementInTheArray.java
@@ -0,0 +1,18 @@
+package Counting_Frequencies_Of_Array_Elements;
+
+public class CountFrequencyOfEachElementInTheArray {
+    public static void main(String[] args) {
+        
+        int [] arr = {10,5,10,15,10,5};
+
+        // int [] arr = {2,2,3,4,4,2};
+
+
+
+    }
+}
+
+
+class Solution {
+    public void countFreq(int n, ar)
+}
